59e326093b2f09ea0e5035eaa17e8b88b8b92896
farhan sharif <farhansharif1410@gmail.com>
2022-08-09 23:47:08 -0230

    13,14,15 no plm solve

--- for-loopProblem.js
+++ for-loopProblem.js
@@ -4,3 +4,22 @@
 for (let i = 1; i <= 10; i++) {
     console.log(i, 'Proti din 8ty video unlook koro.');
 }
+
+// problem 13:
+
+let num = 1;
+while (num <= 10) {
+    console.log(num);
+    num++
+}
+
+let numagain = 10;
+
+while (numagain >= 0) {
+    console.log(numagain, 'This olta number.');
+    numagain--
+}
+
+for (let i = 10; i >= 0; i--) {
+    console.log(i, 'For loop olta.');
+}
